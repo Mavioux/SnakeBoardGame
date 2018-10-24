128e6f2d29b0df7ebcf5e4df13c76f614018caf4
Mavioux <pakmarios@gmail.com>
2018-10-24 19:34:10 +0300

    Initilized Classes and Finished Snake.java

--- src/Snake.java
+++ src/Snake.java
@@ -1,4 +1,51 @@
 
 public class Snake {
-	//test I am Tommino
+	int snakeId;
+	int headId;
+	int tailId;
+	
+	public Snake() {
+		snakeId = 0;
+		headId = 0;
+		tailId = 0;
+	}
+	
+	public Snake(int snakeId, int headId, int tailId) {
+		this.snakeId = snakeId;
+		this.headId = headId;
+		this.tailId = tailId;
+	}
+	
+	public Snake(Snake templateSnake) {
+		this.snakeId = templateSnake.snakeId;
+		this.headId = templateSnake.headId;
+		this.tailId = templateSnake.tailId;
+	}
+	
+	public void setSnakeId(int snakeId) {
+		this.snakeId = snakeId;
+	}
+	
+	public void setHeadId(int headId) {
+		this.headId = headId;
+	}
+	
+	public void setTailId(int tailId) {
+		this.tailId = tailId;
+	}
+	
+	public int getSnakeId() {
+		return snakeId;
+	}
+	
+	public int getHeadId() {
+		return headId;
+	}
+	
+	public int getTailId() {
+		return tailId;
+	}
+	
+	
+	
 }

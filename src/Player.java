5526e1f0f5cb4d205194c49418f2e8624119da36
Mavioux <pakmarios@gmail.com>
2018-10-28 21:24:32 +0200

    Update src/Player.java
    Added setters and getters

--- src/Player.java
+++ src/Player.java
@@ -25,4 +25,47 @@ public class Player {
 		this.score = templatePlayer.score;
 		this.board = new Board(templatePlayer.board);
 	}
+	
+	public void setPlayerId(int playerId) {
+		this.playerId = playerId;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	public void setBoards(Board board) {
+		this.board = board;
+	}
+	
+	public int getPlayerId() {
+		return playerId;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public Board getBoard() {
+		return board;
+	}
+	
+	public int[] move(int id, int die) {
+		
+	}
 }
+
+
+
+
+
+
+
